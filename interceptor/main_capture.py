c2e53946ad893a8d9c98aca7f4ea8494cb5e1681
abhishek singh <abhishek.s14@iiits.in>
2017-03-30 21:00:27 +0530

    get the interceptor working

--- interceptor/main_capture.py
+++ interceptor/main_capture.py
@@ -1,7 +1,8 @@
 from utilities.system_utilities import abort
 import pcapy
 from parameters import *
-
+import sys
+from datetime import datetime
 
 def sniffer_scheduler(session_handler):
     """Summary
@@ -15,7 +16,7 @@ def sniffer_scheduler(session_handler):
     while (True):
         (header, packet) = session_handler.next()
         print("%s: captured %d bytes, truncated to %d bytes" %
-              (datetime.datetime.now(), header.getlen(), header.getcaplen()))
+              (datetime.now(), header.getlen(), header.getcaplen()))
 
 
 def main(argv):
@@ -28,14 +29,18 @@ def main(argv):
         TYPE: Description
     """
     all_devices = pcapy.findalldevs()
+    print(all_devices)
     for device in all_devices:
         print(device)
 
-    device_capture = raw_input("Enter the device to be sniffed")
-    if device in all_devices:
+    device_capture = raw_input("Enter the device to be sniffed: ")
+    if device_capture in all_devices:
         session_handler = pcapy.open_live(
             device, SNAPLEN, PROMISCOUS_MODE, CAPTURE_TIMEOUT)
-        sniffer_scheduler()
+        sniffer_scheduler(session_handler)
     else:
         error_message = 'Device specified is not present in the list'
         abort(-1, error_message)
+
+if __name__ == "__main__":
+    main(sys.argv)
