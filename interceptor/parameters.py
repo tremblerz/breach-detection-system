4c9f02ca35121a641327fea2526c1c1847a41dee
abhishek singh <abhishek.s14@iiits.in>
2017-03-31 14:30:40 +0530

    PEP8 formatting

--- interceptor/parameters.py
+++ interceptor/parameters.py
@@ -1,3 +1,3 @@
 SNAPLEN = 65536  # Maximum number of bytes to capture per packet
 PROMISCOUS_MODE = 1  # value 1 implies true
-CAPTURE_TIMEOUT = 1000  # In milliseconds
+CAPTURE_TIMEOUT = 10000  # In milliseconds
