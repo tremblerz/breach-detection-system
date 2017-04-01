35c7bedc2c103f75705cc000917372a959572d2d
vaishnavm217 <murali.v15@iiits.in>
2017-04-01 15:32:17 +0530

    UPdated Html Pages

--- dashboard/bds/urls.py
+++ dashboard/bds/urls.py
@@ -2,4 +2,8 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
 	url(r'^$',views.index,name="index"),
+	url(r'^confirmed', views.confirmed, name="confirmed"),
+	url(r'^suspicious', views.suspicious, name="suspicious"),
+	url(r'^TopThreats', views.suspicious, name="TopThreats"),
+
 ]
--- dashboard/bds/views.py
+++ dashboard/bds/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
 def index(request):
 	data="some data re!"
 	return render(request,'bds/index.html',{'data':data})
+def confirmed(request):
+	return render(request, 'bds/confirmed.html')
+def suspicious(request):
+	return render(request, 'bds/suspicious.html')
+def TopThreats(request):
+	return render(request, 'bds/TopThreats.html')
 
