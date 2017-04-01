5a1e287b7ea388051b5ca8665cc3d8998eace104
abhishek singh <abhishek.s14@iiits.in>
2017-04-01 19:26:54 +0530

    lazy commit

--- analyzer/external_internal_interaction.py
+++ analyzer/external_internal_interaction.py
@@ -1,7 +1,7 @@
 import multiprocessing
-import IP2Location
+from analyzer.IP2LocationPythonmaster.IP2Location import IP2Location
 import numpy as np
-from utilites.sqlite import *
+#from utilites.sqlite import *
 class ExternalInternalInteraction(multiprocessing.Process):
     """docstring for ExternalInternalInteraction"""
     def __init__(self, arg):
@@ -12,20 +12,20 @@ class ExternalInternalInteraction(multiprocessing.Process):
     	dest_ip = self.arg["IP"]["DST_addr"]
     	return dest_ip
 
-    def parse_src_ip(self):
+    """def parse_src_ip(self):
     	src_ip = self.arg["IP"]["IP_addr"]
-    	return src_ip
+    	return src_ip"""
 
     def find_region(self, ip):
     	dest_ip = self.parse_dest_ip()
-    	IP2locObj = IP2Location.IP2Location()
-    	IP2locObj.open("/home/theprototype/breach-detection-system/analyzer/IP2Location-Python-master/data/IP-COUNTRY.BIN")
+    	IP2locObj = IP2Location()
+    	IP2locObj.open("/home/abhi/Downloads/CourseMaterial/Networking/Information_Security/projects/breach-detection-system/analyzer/IP2LocationPythonmaster/data/IP-COUNTRY.BIN")
     	country = IP2locObj.get_all(dest_ip)
     	return country.country_long
 
     def check_ip_vuln(self, country, ip):
 		newDict = {}
-		f = open('/home/theprototype/breach-detection-system/analyzer/data/vuln_countries.txt', 'r')
+		f = open('/home/abhi/Downloads/CourseMaterial/Networking/Information_Security/projects/breach-detection-system/analyzer/data/vuln_countries.txt', 'r')
 		for line in f:
 			splitLine = line.split()
 			newDict[splitLine[0]] = ' '.join(splitLine[1:])
@@ -42,10 +42,12 @@ class ExternalInternalInteraction(multiprocessing.Process):
     	country = self.find_region(dest_ip)
     	print(country)	
     	vuln = self.check_ip_vuln(country, dest_ip)
-    	src_ip = self.parse_src_ip()
+    	#src_ip = self.parse_src_ip()
     	if vuln == 1:
-    		insert_values(src_ip, dest_ip, 60, 'Potential threat is there')
+            print("[DANGER] Potential threat:")
+    		#insert_values(src_ip, dest_ip, 60, 'Potential threat is there')
     	else:
-    		inser_values(src_ip, dest_ip, 20, 'Not a potential threat')
+            print("Normal traffic")
+    		#inser_values(src_ip, dest_ip, 20, 'Not a potential threat')
         pass
 
